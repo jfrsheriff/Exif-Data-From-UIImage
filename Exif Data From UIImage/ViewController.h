5f1fc4ca9d1c952ba28d803c18167e7521d0b878
Jaffer Sheriff <jaffer-2687@jaffer-2687.local>
2016-11-22 13:19:49 +0530

    Initial Commit

---
+++ Exif Data From UIImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exif Data From UIImage
+//
+//  Created by Jaffer Sheriff U on 22/11/16.
+//  Copyright © 2016 Jaffer Sheriff U. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Exif Data From UIImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exif Data From UIImage
+//
+//  Created by Jaffer Sheriff U on 22/11/16.
+//  Copyright © 2016 Jaffer Sheriff U. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
