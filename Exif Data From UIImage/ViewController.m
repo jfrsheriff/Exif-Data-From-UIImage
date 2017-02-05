1a375dcee56edf8be9aed865ccba4493957a1637
jfrsheriff <jaffer-2687@jaffer-2687.csez.zohocorpin.com>
2017-02-05 18:22:19 +0530

    Demo for getting EXIF data from UIImage

--- Exif Data From UIImage/ViewController.m
+++ Exif Data From UIImage/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <Photos/Photos.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface ViewController ()
 
@@ -16,9 +18,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIButton *openButton = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 200, 200)];
+    [openButton addTarget:self action:@selector(openGallery) forControlEvents:UIControlEventTouchUpInside];
+    [openButton setTitle:@"Open Gallery" forState:UIControlStateNormal];
+    [openButton setBackgroundColor:[UIColor yellowColor]];
+    [openButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [self.view addSubview:openButton];
+    
+    [openButton setCenter:self.view.center];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)openGallery
+{
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [imagePicker setDelegate:self];
+    [self presentViewController:imagePicker animated:YES completion:nil];
+}
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
 
+    NSURL *url = [info objectForKey:UIImagePickerControllerReferenceURL];
+    PHFetchResult *fetchResult = [PHAsset fetchAssetsWithALAssetURLs:@[url] options:nil];
+    PHAsset *asset = fetchResult.firstObject;
+
+    
+    NSString *latitude = [NSString stringWithFormat:@"Lati = %f  Longi = %f", asset.location.coordinate.latitude, asset.location.coordinate.longitude];
+    NSString *dateStr = [NSString stringWithFormat:@" Date =  %@",asset.creationDate];
+    
+    NSString *resultStr = [NSString stringWithFormat:@"%@ \n %@",latitude,dateStr];
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Info" message:resultStr preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }]];
+    
+    [picker dismissViewControllerAnimated:YES completion:^{
+        [self presentViewController:alert animated:YES completion:nil];
+    }];
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(nullable NSDictionary<NSString *,id> *)editingInfo
+{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
